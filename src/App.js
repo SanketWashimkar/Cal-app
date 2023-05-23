350c78d6295b435d36368a85e156452cdda39b57
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-05-23 20:48:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Cal from "./components/Cal/Cal";
+
+function App() {
+  return (
+    <div>
+
+      <Cal />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/common/Button.js
@@ -0,0 +1,21 @@
+import "./Button.css";
+const Button = (props) => {
+  <Button>{props.btName}</Button>;
+
+  const { btName, btFun } = props;
+
+  // const btClick = () =>{
+
+  //   console.log('porps value', props)
+  // }
+
+  return (
+    <div>
+      <button className="bt-cal" onClick={btFun}>
+        {btName}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
