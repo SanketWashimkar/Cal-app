0050ff1d0ba86e3dd63a73c406a5d976fdfa8c12
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-05-27 13:01:34 +0530

    main:6th commit

--- src/components/CalWrapper/CalWrapper.js
+++ src/components/CalWrapper/CalWrapper.js
@@ -1,14 +1,31 @@
 import Button from "../../common/Button";
-
+import './CalWrapper.css'
 const CalWrapper = (props) => {
-  const { allBtArr } = props;
-  const allButton = allBtArr.map((btObj) => {
-    return <Button btName={btObj.value} btFun={btObj.btFun} />;
-  });
 
-    console.log("allButton", allButton);
+    const {allBtArr, oprationArr } = props;
 
-  return <div>{allButton}</div>;
+      const  allButton = allBtArr ?  allBtArr.map((btObj) => {
+        return    <Button key={btObj.value} btName={btObj.value} btFun={btObj.btFun} />
+    }) : [];
+
+    const  allOprationBt = oprationArr ?  oprationArr.map((btObj) => {
+        return    <Button key={btObj.value} btName={btObj.value} btFun={btObj.btFun} />
+    }) : [];
+
+
+    
+
+    return (
+        <div className="main-cal">
+        
+        <div className="bt-name">
+        {allButton}
+        </div>
+       <div className="op-name">
+       {allOprationBt}
+       </div>
+        </div>
+    )
 };
 
 export default CalWrapper;
