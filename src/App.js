a5d976d5d9697af5faeb6acea970cd6c293f86cd
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-05-27 12:58:19 +0530

    main: 2nd commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import "./App.css";
 import Cal from "./components/Cal/Cal";
 
 function App() {
-  return (
-    <div>
-
-      <Cal />
-    </div>
-  );
+  return <Cal />;
 }
 
 export default App;
