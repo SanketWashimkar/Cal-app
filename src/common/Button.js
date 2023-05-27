182f0bafa834727af7389d7337870bcbdea6a7ff
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-05-27 12:59:15 +0530

    main:3rd commit

--- src/common/Button.js
+++ src/common/Button.js
@@ -1,21 +1,17 @@
-import "./Button.css";
+
+import './Button.css';
+
 const Button = (props) => {
-  <Button>{props.btName}</Button>;
+    
+    const { btName, btFun} = props;
 
-  const { btName, btFun } = props;
-
-    // const btClick = () =>{
-
-  //   console.log('porps value', props)
-  // }
-
-  return (
-    <div>
-      <button className="bt-cal" onClick={btFun}>
-        {btName}
-      </button>
-    </div>
-  );
+    return (
+        <div>
+           <button className="bt-cal" onClick={btFun}>
+            {btName}
+           </button>
+        </div>
+    )
 };
 
 export default Button;
