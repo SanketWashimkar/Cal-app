25adbadaf7945293554c53295f8a1b6895784db3
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-05-27 13:00:25 +0530

    main:4th commit

--- src/components/Cal/Cal.js
+++ src/components/Cal/Cal.js
@@ -1,36 +1,81 @@
+import { useState } from "react";
 import CalWrapper from "../CalWrapper/CalWrapper";
+
 const Cal = () => {
+  const [inputVal1, setInputVal1] = useState(0);
+    const [oprationValue, setOprationValue] = useState(0);
+    const [currentOpration,setCurrentOpration] = useState('');
+
   const allBtArr = [
     {
       value: 0,
       btFun: () => {
-        console.log(0);
+        setInputVal1((prevValue) => parseInt(prevValue.toString() + 0));
       },
     },
     {
       value: 1,
       btFun: () => {
-        console.log(1);
+        setInputVal1((prevValue) => parseInt(prevValue.toString() + 1));
       },
     },
     {
       value: 2,
       btFun: () => {
-        console.log(2);
+        const newValue = inputVal1.toString() + 2;
+        setInputVal1(parseInt(newValue));
       },
     },
     {
       value: 3,
       btFun: () => {
-        console.log(3);
+        const newValue = inputVal1.toString() + 3;
+        setInputVal1(parseInt(newValue));
       },
     },
   ];
 
+  const oprationArr = [
+    {
+        value: '+',
+        btFun: () => {
+            setCurrentOpration('+');
+            setOprationValue(inputVal1);
+            setInputVal1(0);
+        }
+    },
+    {
+        value: '-',
+        btFun: () => {
+            setCurrentOpration('-');
+            setOprationValue(inputVal1);
+            setInputVal1(0);
+        }
+    },
+    {
+        value: '=',
+        btFun: () => {
+            if(currentOpration === '+'){
+
+                setInputVal1((prevValue) => {
+                    return prevValue + oprationValue
+                })
+
+            }else if(currentOpration === '-'){
+                setInputVal1((prevValue) => {
+                    return oprationValue - prevValue; 
+                })
+            }
+        }
+    }
+  ];
   return (
     <div>
-      <p>This is Cal js</p>
-          <CalWrapper allBtArr={allBtArr} />
+      <p> This is cal js {inputVal1} </p>
+      <p>   {oprationValue} {currentOpration}</p>
+
+
+      <CalWrapper allBtArr={allBtArr} oprationArr={oprationArr}/>
     </div>
   );
 };
